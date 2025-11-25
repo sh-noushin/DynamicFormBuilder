16033f106a251155b7bb3f6a63f019812c80717f
Nooshin Shokrollahi <nshokrollahi951@gmail.com>
2025-11-25 20:32:08 +0100

    Add API base URL configuration and update app configuration

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { API_BASE_URL } from './core/services/api-service';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    { provide: API_BASE_URL, useValue: environment.apiBaseUrl }
   ]
 };
